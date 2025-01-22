6609fa5fddceda20b06a6a22b6d312e50b5894be
alexcl11 <159544265+alexcl11@users.noreply.github.com>
2025-01-22 14:01:44 +0100

    update

--- web/views/pelicula.view.php
+++ web/views/pelicula.view.php
@@ -5,7 +5,7 @@
     <div class="container">
         <h2>Título película</h2>
         <div class="row">
-            <div class="col-4 d-flex justify-content-end">
+            <div class="col-4">
                 <img src="views/images/cartel1.webp" alt="" class="img-fluid">
             </div>
             <div class="col-8 col-md-3 order-md-2">
@@ -33,6 +33,26 @@
                     <h5>SINOPSIS</h5>
                     <p>Precuela de 'El rey león' (2019). Cuenta la historia de origen del padre de Simba,
                         Mufasa, explorando su infancia al crecer con su hermano Scar.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-8">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Home</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Profile</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Contact</button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">...</div>
+                    <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
+                    <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">...</div>
                 </div>
             </div>
         </div>
