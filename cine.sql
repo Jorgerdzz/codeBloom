b8983bd9ca5015e7e6560da1041935f8c3f964e2
Truido <181877233+Truido@users.noreply.github.com>
2025-01-23 18:57:13 +0100

    Se ha añadido el script con la estructura básica de la base de datos

---
+++ cine.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 23-01-2025 a las 17:52:24
+-- Versión del servidor: 10.11.8-MariaDB-0ubuntu0.24.04.1
+-- Versión de PHP: 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Clientes`
+--
+
+CREATE TABLE `Clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `correo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Entradas`
+--
+
+CREATE TABLE `Entradas` (
+  `id` int(11) NOT NULL,
+  `asiento` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_pase` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Pases`
+--
+
+CREATE TABLE `Pases` (
+  `id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `id_pelicula` int(11) NOT NULL,
+  `id_sala` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Peliculas`
+--
+
+CREATE TABLE `Peliculas` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `fecha_estreno` date NOT NULL,
+  `duracion` int(11) NOT NULL,
+  `reparto` varchar(255) NOT NULL,
+  `calificacion_edad` int(11) NOT NULL,
+  `sinopsis` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `Peliculas`
+--
+
+INSERT INTO `Peliculas` (`id`, `titulo`, `direccion`, `fecha_estreno`, `duracion`, `reparto`, `calificacion_edad`, `sinopsis`) VALUES
+(1, 'Sonic 3 - La película', 'Jeff Fowler', '2024-12-25', 110, 'Jim Carrey, Ben Schwartz, Idris Elba, Keanu Reeves', 7, 'Sonic, Knuckles y Tails se reúnen para enfrentarse a un nuevo y poderoso adversario, Shadow, un misterioso villano cuyos poderes no se parecen a nada de lo que nuestros héroes han conocido hasta ahora. Con sus facultades superadas en todos los sentidos, el Equipo Sonic tendrá que establecer una insólita alianza con la esperanza de detener a Shadow y proteger el planeta.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Salas`
+--
+
+CREATE TABLE `Salas` (
+  `id` int(11) NOT NULL,
+  `butacas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `Salas`
+--
+
+INSERT INTO `Salas` (`id`, `butacas`) VALUES
+(1, 200),
+(2, 200);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Clientes`
+--
+ALTER TABLE `Clientes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `clientes_correo_unique` (`correo`);
+
+--
+-- Indices de la tabla `Entradas`
+--
+ALTER TABLE `Entradas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_pase` (`id_pase`);
+
+--
+-- Indices de la tabla `Pases`
+--
+ALTER TABLE `Pases`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Pases_ibfk_1` (`id_pelicula`),
+  ADD KEY `Pases_ibfk_2` (`id_sala`);
+
+--
+-- Indices de la tabla `Peliculas`
+--
+ALTER TABLE `Peliculas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `Salas`
+--
+ALTER TABLE `Salas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Clientes`
+--
+ALTER TABLE `Clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Entradas`
+--
+ALTER TABLE `Entradas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Pases`
+--
+ALTER TABLE `Pases`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Peliculas`
+--
+ALTER TABLE `Peliculas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `Entradas`
+--
+ALTER TABLE `Entradas`
+  ADD CONSTRAINT `Entradas_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `Clientes` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `Entradas_ibfk_2` FOREIGN KEY (`id_pase`) REFERENCES `Pases` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `Pases`
+--
+ALTER TABLE `Pases`
+  ADD CONSTRAINT `Pases_ibfk_1` FOREIGN KEY (`id_pelicula`) REFERENCES `Peliculas` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `Pases_ibfk_2` FOREIGN KEY (`id_sala`) REFERENCES `Salas` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
