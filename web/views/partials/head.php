0736b2cfb511a0efb0b0b0559948ff05a0ba958c
alexcl11 <159544265+alexcl11@users.noreply.github.com>
2025-01-31 10:42:14 +0100

    Ajustes nav e implementación favicon

--- web/views/partials/head.php
+++ web/views/partials/head.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cine Eternum</title>
-    <link rel="stylesheet" href="<?=BASE_PATH; ?>/views/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?=BASE_PATH; ?>/views/bootstrap/css/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="<?=BASE_PATH; ?>/views/css/style.css">
+    <link rel="icon" type="image/x-icon" href="<?= BASE_PATH; ?>/views/images/favicon.png">
+    <link rel="stylesheet" href="<?= BASE_PATH; ?>/views/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= BASE_PATH; ?>/views/bootstrap/css/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="<?= BASE_PATH; ?>/views/css/style.css">
 </head>
 
 <body data-page="<?= $page ?>">
--- web/views/partials/nav.php
+++ web/views/partials/nav.php
@@ -1,39 +1,18 @@
-    <nav class="fs-4 mb-3">
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <a class="navbar-brand" href="<?= BASE_PATH . '/'; ?>">
-                    <img class="img-fluid" style="width: 15rem;" src="<?= BASE_PATH . '/views/images/logo.png'; ?>" alt="">
-                </a>
-                <button class="navbar-toggler btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav ms-auto">
-                        <a class="nav-link" href="<?= BASE_PATH; ?>#cartelera">Cartelera</a>
-                        <a class="nav-link" href="<?= BASE_PATH . '/contacto'; ?>">Contacto</a>
-                        <a class="nav-link" href="<?= BASE_PATH . '/sobre-nosotros'; ?>">Sobre nosotros</a>
-                        <a class="nav-link" href="<?= BASE_PATH . '/iniciar-sesion'; ?>">Iniciar sesión</a>
-                    </div>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="<?= BASE_PATH . '/'; ?>">
+                <img class="img-fluid" style="width: 15rem;" src="<?= BASE_PATH . '/views/images/logo.png'; ?>" alt="">
+            </a>
+            <button class="navbar-toggler btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto">
+                    <a class="nav-link" href="<?= BASE_PATH; ?>#cartelera">Cartelera</a>
+                    <a class="nav-link" href="<?= BASE_PATH . '/contacto'; ?>">Contacto</a>
+                    <a class="nav-link" href="<?= BASE_PATH . '/sobre-nosotros'; ?>">Sobre nosotros</a>
+                    <a class="nav-link" href="<?= BASE_PATH . '/iniciar-sesion'; ?>">Iniciar sesión</a>
                 </div>
             </div>
-        </nav>
-
-
-        <!-- <ul class="nav navbar-expand-md justify-content-center">
-            <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="<?= BASE_PATH; ?>">LOGO</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= BASE_PATH; ?>#cartelera">Cartelera</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= BASE_PATH . '/contacto'; ?>">Contacto</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= BASE_PATH . '/sobre-nosotros'; ?>">Sobre nosotros</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= BASE_PATH . '/iniciar-sesion'; ?>">Iniciar sesión</a>
-            </li>
-        </ul> -->
+        </div>
     </nav>
