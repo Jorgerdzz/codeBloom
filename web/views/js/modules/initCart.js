a0202dfeb1eaac57c1e9dfc139ab86176dc2c18c
alexcl11 <159544265+alexcl11@users.noreply.github.com>
2025-02-18 13:58:48 +0100

    Update initCart.js

--- web/views/js/modules/initCart.js
+++ web/views/js/modules/initCart.js
@@ -1,17 +1,17 @@
 export default function initCart() {
-  //   const cart = JSON.parse(localStorage.getItem("cart"));
-  //   const cartBody = document.getElementById("cart-body");
-  //   if (cart === null || cart.length === 0) {
-  //     const h3 = document.createElement("h3");
-  //     h3.innerText = "La cesta está vacía";
-  //     cartBody.appendChild(h3);
-  //   } else {
-  //     for (const item of cart) {
-  //       const ticket = `
-  //         <h3>${item["movieTitle"]}</h3>
-  //         <p>${item["day"]} | ${item["time"]}</p>
-  //         `;
-  //       cartBody.innerHTML += ticket;
-  //     }
-  //   }
+  const cart = JSON.parse(localStorage.getItem("cart"));
+  const cartBody = document.getElementById("cart-body");
+  if (cart === null || cart.length === 0) {
+    const h3 = document.createElement("h3");
+    h3.innerText = "La cesta está vacía";
+    cartBody.appendChild(h3);
+  } else {
+    for (const item of cart) {
+      const ticket = `
+          <h3>${item["movieTitle"]}</h3>
+          <p>${item["day"]} | ${item["time"]}</p>
+          `;
+      cartBody.innerHTML += ticket;
+    }
+  }
 }
