0ab58f50af8a81ed3abc76139265ee3bece03544
alexcl11 <159544265+alexcl11@users.noreply.github.com>
2025-02-18 13:56:37 +0100

    Se ha implementado la lógica del carrito

--- web/views/cesta.view.php
+++ web/views/cesta.view.php
@@ -2,9 +2,25 @@
 <?php require 'views/partials/nav.php'; ?>
 
 <main>
-    <div class="container">
-        <h1 class="text-center">Mi cesta</h1>
-        
+    <h1 class="text-center">Mi cesta</h1>
+    <div id="cart-body" class="text-center">
+        <!-- 
+        <div class="row d-flex justify-content-center border">
+            <div class="col border">
+                <img class="img-fluid" src="views/images/posters/1.webp" alt="">
+            </div>
+            <div class="col">
+                <div class="col flex-column">
+                    <h3>Título</h3>
+                    <p>20-01-2025 | 16:30</p>
+                    <p>Sala 1</p>
+                    <p>8 €</p>
+                    <input type="number" value="2">
+                    <br>
+                    <button>Eliminar</button>
+                </div>
+            </div>
+        </div> -->
     </div>
 </main>
 
