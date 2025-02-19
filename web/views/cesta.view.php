8d029f9c759a52121a1edc8b09c0b01369039941
alexcl11 <159544265+alexcl11@users.noreply.github.com>
2025-02-19 09:38:46 +0100

    Se ha implementado la vista del carrito

--- web/views/cesta.view.php
+++ web/views/cesta.view.php
@@ -3,24 +3,7 @@
 
 <main>
     <h1 class="text-center">Mi cesta</h1>
-    <div id="cart-body" class="text-center">
-        <!-- 
-        <div class="row d-flex justify-content-center border">
-            <div class="col border">
-                <img class="img-fluid" src="views/images/posters/1.webp" alt="">
-            </div>
-            <div class="col">
-                <div class="col flex-column">
-                    <h3>Título</h3>
-                    <p>20-01-2025 | 16:30</p>
-                    <p>Sala 1</p>
-                    <p>8 €</p>
-                    <input type="number" value="2">
-                    <br>
-                    <button>Eliminar</button>
-                </div>
-            </div>
-        </div> -->
+    <div id="cart-body" class="d-flex flex-column align-items-center">
     </div>
 </main>
 
--- web/views/js/modules/initCart.js
+++ web/views/js/modules/initCart.js
@@ -7,11 +7,26 @@ export default function initCart() {
     cartBody.appendChild(h3);
   } else {
     for (const item of cart) {
-      const ticket = `
-          <h3>${item["movieTitle"]}</h3>
-          <p>${item["day"]} | ${item["time"]}</p>
-          `;
-      cartBody.innerHTML += ticket;
+      cartBody.innerHTML += `
+          <div class="card mb-3" style="max-width: 450px;">
+            <div class="row g-0">
+                <div class="col-4">
+                    <img src="${item["poster"]}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-8 align-content-center">
+                    <div class="card-body">
+                            <h5 class="card-title">${item["movieTitle"]}</h5>
+                            <p class="card-text">${item["day"]} | ${item["time"]}</p>
+                            <p class="card-text">Sala ${item["screen"]}</p>
+                            <p class="card-text">Precio: ${item["price"]} €</p>
+                            <label for="quantity">Cantidad:</label>
+                            <input type="number" value="${item["quantity"]}" style="max-width: 3rem;" id="quantity">
+                            <i class="bi bi-trash3-fill ms-2"></i> 
+                    </div>
+                </div>
+            </div>
+        </div>          
+      `;
     }
   }
 }
