fa9670ee628cfe84ab7a23489350087855157ee7
Truido <181877233+Truido@users.noreply.github.com>
2025-02-20 17:47:43 +0100

    Ajustes footer

--- web/views/partials/footer.php
+++ web/views/partials/footer.php
@@ -1,20 +1,20 @@
 <footer>
     <div class="container py-3 my-4 border-top">
         <div class="row justify-content-between align-items-center">
-            <div class="col-12 col-md-4 d-flex justify-content-center">
-                <a href="/">
+            <div class="col-12 col-md-4 d-flex justify-content-center order-md-1">
+                <a href="<?= BASE_PATH . '/'; ?>">
                     <img src="<?= BASE_PATH; ?>/views/images/logo.png" alt="" style="width: 15rem;">
                 </a>
             </div>
-            <div class="col-12 col-md-4 d-flex justify-content-center">
-                <span class="mb-3 mb-md-0">© 2025 Cine Eternum</span>
+            <div class="col-12 col-md-4 d-flex justify-content-center order-md-3">
+                <a href="#"><i class="bi bi-tiktok nav-link m-3"></i></a>
+                <a href="#"><i class="bi bi-youtube nav-link m-3"></i></a>
+                <a href="#"><i class="bi bi-instagram nav-link m-3"></i></a>
+                <a href="#"><i class="bi bi-facebook nav-link m-3"></i></a>
             </div>
-            <div class="col-12 col-md-4 d-flex justify-content-center">
-                <a href=""><i class="bi bi-twitter-x nav-link m-3"></i></a>
-                <a href=""><i class="bi bi-facebook nav-link m-3"></i></a>
-                <a href=""><i class="bi bi-instagram nav-link m-3"></i></a>
+            <div class="col-12 col-md-4 d-flex justify-content-center order-md-2">
+                <span>© 2025 Cine Eternum</span>
             </div>
-
         </div>
     </div>
 </footer>
