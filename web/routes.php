847cf5a03fa6434a41ba216073294ae173e3c884
alexcl11 <159544265+alexcl11@users.noreply.github.com>
2025-02-20 10:54:26 +0100

    Implementado el sitemap

--- web/routes.php
+++ web/routes.php
@@ -3,12 +3,12 @@ return [
     '/' => 'controllers/home.php',
     '/pelicula' => 'controllers/pelicula.php',
     '/contacto' => 'controllers/contacto.php',
-    '/iniciar-sesion' => 'controllers/Session/iniciar-sesion.php',    
+    '/iniciar-sesion' => 'controllers/Session/iniciar-sesion.php',
     '/mi-cuenta' => 'controllers/Session/mi-cuenta.php',
     '/mi-cuenta/modificar' => 'controllers/Session/modificar.php',
     '/mi-cuenta/eliminar' => 'controllers/Session/eliminar.php',
     '/mi-cuenta/cerrar-sesion' => 'controllers/Session/cerrar-sesion.php',
     '/crear-cuenta' => 'controllers/Registration/crear-cuenta.php',
     '/bienvenida' => 'controllers/Registration/bienvenida.php',
-    '/cesta' => 'controllers/cesta.php',
+    '/cesta' => 'controllers/cesta.php'
 ];
