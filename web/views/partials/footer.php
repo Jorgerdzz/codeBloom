e033bf2f9191c3148a6595d3328896eabb94b35a
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-02-20 09:43:52 +0100

    Implementacion footer

--- web/views/partials/footer.php
+++ web/views/partials/footer.php
@@ -1,6 +1,39 @@
-<footer></footer>
-<script src="<?=BASE_PATH; ?>/views/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script type="module" src="<?=BASE_PATH; ?>/views/js/script.js"></script>
+<!-- <div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <img src="<?= BASE_PATH . '/views/images/logo.png'; ?>" alt="" style="width: 15rem;">
+    </a>
+    <span class="mb-3 mb-md-0">© 2025 Cine Eternum, Inc</span>
+    <ul class="nav list-unstyled d-flex">
+        <li class="ms-3"><a class="nav-link" href="#"><i class="bi bi-twitter-x"></i></a></li>
+        <li class="ms-3"><a class="nav-link" href="#"><i class="bi bi-facebook"></i></a></li>
+        <li class="ms-3"><a class="nav-link" href="#"><i class="bi bi-instagram"></i></a></li>
+    </ul>
+  </footer>
+</div> -->
+
+<footer>
+    <div class="container py-3 my-4 border-top">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-12 col-md-4 d-flex justify-content-center">
+                <a href="/">
+                    <img src="<?= BASE_PATH; ?>/views/images/logo.png" alt="" style="width: 15rem;">
+                </a>
+            </div>
+            <div class="col-12 col-md-4 d-flex justify-content-center">
+                <span class="mb-3 mb-md-0">© 2025 Cine Eternum</span>
+            </div>
+            <div class="col-12 col-md-4 d-flex justify-content-center">
+                <a href=""><i class="bi bi-twitter-x nav-link m-3"></i></a>
+                <a href=""><i class="bi bi-facebook nav-link m-3"></i></a>
+                <a href=""><i class="bi bi-instagram nav-link m-3"></i></a>
+            </div>
+
+        </div>
+    </div>
+</footer>
+<script src="<?= BASE_PATH; ?>/views/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script type="module" src="<?= BASE_PATH; ?>/views/js/script.js"></script>
 </body>
 
 </html>
