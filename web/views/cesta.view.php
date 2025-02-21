e7f0b485296d661530f84f6107166de02b0341e9
alexcl11 <159544265+alexcl11@users.noreply.github.com>
2025-02-21 10:27:57 +0100

    Correción formato entrada

--- web/views/cesta.view.php
+++ web/views/cesta.view.php
@@ -4,9 +4,8 @@
 <main>
     <h1 class="text-center">Mi cesta</h1>
     <div class="container text-center">
-        <div id="cart-body" class="d-flex flex-column align-items-center"></div>
+        <div id="cart-body" class="d-flex flex-column align-items-center text-start"></div>
         <button id="buy-button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#thanks-modal" style="display: none">Finalizar compra</button>
-
     </div>
     <div class="modal" id="thanks-modal" tabindex="-1">
         <div class="modal-dialog">
@@ -15,7 +14,7 @@
                     <h5 class="modal-title">¡Compra realizada con éxito!</h5>
                 </div>
                 <div class="modal-body text-center">
-                    <p>Esperamos que disfrutes esta experiencia en Cine Eternum.</p>
+                    <p>Esperamos que disfrutes esta experiencia en Cine Eternum</p>
                 </div>
                 <div class="modal-footer">
                     <a class="btn btn-primary" href="<?= BASE_PATH . '/' ?>">Volver</a>
